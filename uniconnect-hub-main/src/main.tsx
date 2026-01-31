f8c23798aa8fbb47d6ee1cef753fc7fb917a5810
SHAIK SIDDIQ ALI <24pa1a45b1@vishnu.edu.in>
2026-01-31 10:39:23 +0530

    Update main.tsx

--- uniconnect-hub-main/src/main.tsx
+++ uniconnect-hub-main/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 // Global runtime handlers to help catch silent failures in the browser
 window.addEventListener('error', (ev) => {
